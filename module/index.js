79a34893a24a2cdc86bc22ee2ee918360f20467c
Hervé Guétin <herve.guetin@gmail.com>
2024-03-15 18:35:53 +0100

    Rewrites

--- module/index.js
+++ module/index.js
@@ -2,19 +2,35 @@
  * @author Hervé Guétin <www.linkedin.com/in/herveguetin>
  */
 
+let zermattConfig = {}
+
 async function loadModule(configModule) {
     let importModule = await import(configModule.path)
+    const rewrite = zermattConfig.rewrites.find(rewrite => rewrite.name === configModule.name)
+    if (rewrite) {
+        const importRewrite = await import(rewrite.path)
+        const moduleDefault = importModule.default
+        const rewriteDefault = importRewrite.default
+        importModule = {
+            default: {
+                ...moduleDefault,
+                ...rewriteDefault
+            }
+        }
+    }
     return {
         name: configModule.name,
         default: importModule.default
     }
 }
 
-function loadModules(modules) {
-    const promises = modules.map(configModule => loadModule(configModule))
+function loadModules(config) {
+    zermattConfig = config
+    const promises = zermattConfig.modules.map(configModule => loadModule(configModule))
     return Promise.all(promises)
 }
 
 export default {
     loadAll: loadModules
 }
+
