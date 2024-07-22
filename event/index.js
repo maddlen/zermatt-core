09cb6cc30abcd206ada5814f37ab41d531bcaadc
Hervé Guétin <herve.guetin@gmail.com>
2024-07-22 12:02:04 +0200

    Event - Fixed missing event detail when using Event.waitFor

--- event/index.js
+++ event/index.js
@@ -39,6 +39,6 @@ const waitFor = function (events, callback) {
 
 export default {
     listen: (event, callback) => document.addEventListener(event, callback),
-    dispatch: (event, data) => document.dispatchEvent(new CustomEvent(event, data)),
+    dispatch: (event, data) => document.dispatchEvent(new CustomEvent(event, {detail: data})),
     waitFor: waitFor
 }
