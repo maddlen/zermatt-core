55c2c6ceb6ce48d436477ea868a740f233e81ded
Hervé Guétin <herve.guetin@gmail.com>
2024-07-22 22:34:29 +0200

    Events - "on" and "once"

--- app/init.js
+++ app/init.js
@@ -19,12 +19,12 @@ client.axios().defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 let config = {}
 
 const listen = function () {
-    Event.on('zermatt:kickoff', () => {
+    Event.once('zermatt:kickoff', () => {
         Translation.init()
         FormKey.fetch()
     })
-    Event.on('zermatt:translation:init', () => Module.init(config))
-    Event.on(['zermatt:translation:init', 'zermatt:module:init'], () => {
+    Event.once('zermatt:translation:init', () => Module.init(config))
+    Event.once(['zermatt:translation:init', 'zermatt:module:init'], () => {
         Event.dispatch('zermatt:init')
         Alpine.start()
         Event.dispatch('zermatt:ready')
--- event/index.js
+++ event/index.js
@@ -3,10 +3,11 @@
  */
 const EventGroup = class {
 
-    constructor(events, callback) {
+    constructor(events, always, callback) {
         this.completedEvents = []
         this.events = events
         this.callback = callback
+        this.always = always
 
         this.listen()
     }
@@ -23,24 +24,29 @@ const EventGroup = class {
     }
 
     isComplete() {
-        if (this.events.length === this.completedEvents.length) {
-            this.events.map(event => document.removeEventListener(event, this.onEvent.bind(this)))
+        if (!this.always && this.events.length === this.completedEvents.length) {
             const completedEvents = this.completedEvents.length > 1 ? this.completedEvents : this.completedEvents[0]
+            this.events.map(event => document.removeEventListener(event, this.onEvent.bind(this)))
             this.callback(completedEvents)
+        }
+        if (this.always) {
+            const completedEvent = this.completedEvents.length > 1 ? this.completedEvents[this.completedEvents.length - 1] : this.completedEvents[0]
+            this.callback(completedEvent)
         }
     }
 }
 
-const waitFor = function (events, callback) {
+const createEventGroup = function (events, callback, always) {
     const groupContainer = {}
     events = Array.isArray(events) ? events : [events]
-    groupContainer.eventGroup = new EventGroup(events, function (completedEvents) {
+    groupContainer.eventGroup = new EventGroup(events, always, function (completedEvents) {
         callback(completedEvents)
         delete groupContainer.eventGroup
     })
 }
 
 export default {
-    on: waitFor,
+    once: (events, callback) => createEventGroup(events, callback, false),
+    on: (events, callback) => createEventGroup(events, callback, true),
     dispatch: (event, data) => document.dispatchEvent(new CustomEvent(event, {detail: data}))
 }
